4b865b4a0f04803a4b4bd2f2c70f5a1b2cf50f56
Brandan Lennox <brandan@bclennox.com>
2018-04-18 01:42:07 -0400

    Normalize username case before authenticating.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def self.authenticate(username, password)
-    find_by(username: username)&.authenticate(password)
+    find_by(username: username&.downcase)&.authenticate(password)
   end
 
   def name
